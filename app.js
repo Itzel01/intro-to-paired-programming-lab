3371c3da591837c7d9fea5f0fda49fe9e5be98e8
EC2 Default User <ec2-user@ip-172-31-17-210.us-east-2.compute.internal>
2020-09-28 19:29:49 +0000

    updated

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ function sayHello(name){
 
 function letterCount(name){
 
-}
+  if (name.length > 5){
+    console.log(`${name}, your name has ${name.length} letters. That's a pretty long name!`)}
+    else {
+      console.log(`${name}, your name has ${name.length} letters. That name's not that long!`);
+      
+    }
+  }
+ // 
 
 function greetPeople(names){
 
@@ -24,6 +31,8 @@ function kebabToTitleCase(filenames){
 //Write your test cases below this comment
 sayHello('jeff'); // expect: Hello, jeff
 sayHello('Itzy'); // expect: Hello, Itzy
+letterCount('jeffgdbd')
+letterCount('Itzy')
 
 //do not editing anything below this comment.
 module.exports = {
